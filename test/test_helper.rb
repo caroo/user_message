580740ec9e0ddcf0d01203ed7ee30b587264cd7b
Andreas Riemer <andreas.riemer@gmail.com>
2010-03-12 16:13:22 +0000

    CHANGED Ueberall im trunk require 'activesupport' und require 'activerecord' durch require 'active_support' und require 'active_record' ersetzt
    refs #2531353
    reviewed ffrank
    
    git-svn-id: https://svn.pkw.de/pkw.de/trunk/gems/user_message@9343 a0138c44-f643-4d49-8908-3bd568d7ed4a

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,8 @@ require 'stringio'
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/user_message'
 
-require 'activesupport' # for I18n
-require 'activerecord' # for ActiveRecord::Errors
+require 'active_support' # for I18n
+require 'active_record' # for ActiveRecord::Errors
 require 'actionmailer' # for ActionController::Flash
 require 'mocha'
 
