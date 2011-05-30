5a823c1c0892a4d790e317c248431bef0977b82e
Andreas Riemer <andreas.riemer@gmail.com>
2011-05-30 16:24:31 +0200

    Bugfix release init.rb location
    rails < 3 seems to not load init.rb from the gem root path, so
    we moved it to rails subfolder

---
+++ rails/init.rb
@@ -0,0 +1,10 @@
+require File.join(File.dirname(__FILE__), *%w[.. lib user_message])
+require 'active_support'
+require 'action_controller'
+require 'action_view'
+require 'action_view/paths'
+
+view_path = File.expand_path(File.join(File.dirname(__FILE__), *%w[.. lib views]))
+ActionController::Base.view_paths << view_path unless ActionController::Base.view_paths.include?(view_path)
+translation_path = File.expand_path(File.join(File.dirname(__FILE__), *%w[.. locale *.yml]))
+I18n.config.load_path += Dir[translation_path].map{|path| path}
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,6 @@ require 'active_support' # for I18n
 require 'active_record' # for ActiveRecord::Errors
 require 'action_controller' # for ActionController::Flash
 
-require File.dirname(__FILE__) + '/../init.rb'
+require File.dirname(__FILE__) + '/../rails/init.rb'
 
 ActiveRecord::Base # Wenn diese Zeile entfernt wird, dann wird ActiveRecord::Errors nicht gefunden.
