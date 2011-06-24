17e051ea004d1edd29b6d7c940dcceb940c18545
Andreas Riemer <andreas.riemer@gmail.com>
2011-06-24 13:03:32 +0200

    Rewrites test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,14 @@ require 'test/unit'
 require 'mocha'
 
 require 'stringio'
+require 'rails'
 require 'action_controller' # for ActionController::Flash
 require 'active_model/errors'
 require 'user_message'
-UserMessage::Railtie.initializers.map(&:block).each(&:call)
+
+module UserMessage
+  class TestApplication < Rails::Application
+    config.active_support.deprecation :log
+  end
+end
+UserMessage::TestApplication.initialize!
